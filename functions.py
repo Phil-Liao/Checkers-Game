61aab629f8f6550ece536a11512060addf1775e1
Phil <philliao@protonmail.com>
2022-07-06 11:01:44 +0800

    Removing Unused Function [FINISHED]

--- functions.py
+++ functions.py
@@ -12,14 +12,3 @@ def key_in_dict(dict, value):
         if dict[i+1] == value:
             #print(f"The key is {i+1}")
             return i + 1
-
-def closest_to_point(x, y, GAME_WIDTH, GAME_HEIGHT):
-    for x_axis in range(0, float(GAME_WIDTH), float(GAME_WIDTH/8)):
-        if 0 < x - x_axis < GAME_WIDTH / 8:
-            return_x = x_axis
-            break
-    for y_axis in range(0, GAME_HEIGHT, GAME_HEIGHT/8):
-        if 0 < y - y_axis < GAME_HEIGHT / 8:
-            return_y = y_axis
-            break
-    return return_x, return_y
